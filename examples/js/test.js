8cb6fc95a909c6d3adb8a8e56cb779a7a39d4f4b
Alexandr <yakovlev@primepix.ru>
2013-09-06 17:33:45 +0400

    edit test
    Signed-off-by: Alexandr <yakovlev@primepix.ru>

--- examples/js/test.js
+++ examples/js/test.js
@@ -1,15 +1,30 @@
 (function($){
     $(function(){
-        var query = {
-            type: 'city',
-            name: 'арх',
-            limit: 10
-        };
+        var kladr = $(document.getElementById('kladr_autocomplete'));        
+        var input = $('input');
         
-        $.kladr.api(query, function(objs){
-           for(var i in objs) {
-               console.log(objs[i].name);
-           }
+        var Position = function(){
+            var inputOffset = input.offset();
+            kladr.find('ul').css({
+               top:  inputOffset.top+input.outerHeight() + 'px',
+               left: inputOffset.left,
+               width: (input.outerWidth() - 2) + 'px'
+            });
+        }
+        
+        input.keydown(function(){
+            kladr.find('ul').slideDown(100);
         });
+        
+        kladr.find('li, a').click(function(){
+           var a = $(this);
+           if(a.is('li')) a = a.find('a');
+           input.val(a.attr('data-val'));
+           a.closest('ul').hide();
+           return false;
+        });
+        
+        $(window).resize(Position);
+        Position();        
     }); 
 })(jQuery);
