2659f157230854599d9380537725ab1fb81563cc
Alexandr <yakovlev@primepix.ru>
2013-09-09 13:12:00 +0400

    add sketch plugin jquery.kladr
    Signed-off-by: Alexandr <yakovlev@primepix.ru>

--- examples/js/test.js
+++ examples/js/test.js
@@ -1,30 +1,5 @@
 (function($){
     $(function(){
-        var kladr = $(document.getElementById('kladr_autocomplete'));        
-        var input = $('input');
-        
-        var Position = function(){
-            var inputOffset = input.offset();
-            kladr.find('ul').css({
-               top:  inputOffset.top+input.outerHeight() + 'px',
-               left: inputOffset.left,
-               width: (input.outerWidth() - 2) + 'px'
-            });
-        }
-        
-        input.keydown(function(){
-            kladr.find('ul').slideDown(100);
-        });
-        
-        kladr.find('li, a').click(function(){
-           var a = $(this);
-           if(a.is('li')) a = a.find('a');
-           input.val(a.attr('data-val'));
-           a.closest('ul').hide();
-           return false;
-        });
-        
-        $(window).resize(Position);
-        Position();        
+        $('input').kladr();
     }); 
 })(jQuery);
--- jquery.kladr.js
+++ jquery.kladr.js
@@ -51,3 +51,81 @@
         });
     };
 })(jQuery);
+
+(function( $, undefined ){
+    $.fn.kladr = function( options ){
+        var input = this;
+        var ac = null;
+        
+        var create = function(){
+            var container = $(document.getElementById('kladr_autocomplete'));
+            if(!container.length){
+                container = $('<div id="kladr_autocomplete"></div>').appendTo('body');
+            }
+            
+            ac = $('<ul style="display: none;"></ul>').appendTo(container);            
+            ac.on('click', 'li, a', itemClick);
+        };
+        
+        var position = function(){
+            var inputOffset = input.offset();
+            
+            ac.css({
+               top:  inputOffset.top+input.outerHeight() + 'px',
+               left: inputOffset.left
+            });
+            
+            var differ = ac.outerWidth() - ac.width();
+            ac.width(input.outerWidth() - differ);
+        };
+        
+        var itemClick = function(){
+            var a = $(this);
+            if(a.is('li')) a = a.find('a');
+            input.val(a.attr('data-val'));
+            input.data('kladr-object', a.data('kladr-object'));
+            a.closest('ul').hide();
+            return false;
+        };
+        
+        var render = function(objs){        
+            ac.empty();            
+            for(var i in objs){
+                var obj = objs[i];
+                
+                var a = $('<a data-val="'+value(obj)+'">'+label(obj)+'</a>');
+                a.data('kladr-object', obj);
+                                
+                var li = $('<li></li>').append(a);                
+                li.appendTo(ac);
+            }
+        };
+        
+        var label = function(obj){
+            return obj.typeShort+'. '+obj.name;
+        };
+        
+        var value = function(obj){
+            return obj.name;
+        };
+        
+        var source = function(query){
+            return [
+                {typeShort: 'г', name: 'Архангельск'},
+                {typeShort: 'пгт', name: 'Архара'},
+                {typeShort: 'пгт', name: 'Архиповка'},
+                {typeShort: 'п', name: 'Архангельский'},
+            ];
+        };
+        
+        create();
+        $(window).resize(position);
+        input.keydown(function(){
+            var query = input.val();
+            var objs = source(query);
+            render(objs);
+            position();           
+            ac.slideDown(100);
+        });
+    };
+})(jQuery);
