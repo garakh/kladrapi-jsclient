bae7c8ae04de46b7b8794cb869f130114d239cc8
Alexandr <yakovlev@primepix.ru>
2013-09-12 12:31:09 +0400

    edit examples
    Signed-off-by: Alexandr <yakovlev@primepix.ru>

--- examples/js/test.js
+++ examples/js/test.js
@@ -1,9 +1,25 @@
-(function($){
-    $(function(){
-        $('input').kladr({
-            token: 'token',
-            key: 'key',
-            type: $.kladr.type.city
-        });
-    }); 
-})(jQuery);
+$(function(){
+    var cityInput = $('[name="city"]');
+    var streetInput = $('[name="street"]');
+    
+    cityInput.kladr({
+        token: 'token',
+        key: 'key',
+        type: $.kladr.type.city,
+        select: function(obj){
+            streetInput.kladr('parentId', obj.id);
+        },
+        check: function(obj){
+            if(obj){
+                streetInput.kladr('parentId', obj.id);
+            }
+        }
+    });
+
+    streetInput.kladr({
+        token: 'token',
+        key: 'key',
+        type: $.kladr.type.street,
+        parentType: $.kladr.type.city
+    });
+});
