f593f379fe1df0345d9551569f2c0dd7cee58c00
Alexandr <xescoder@gmail.com>
2014-09-14 22:00:50 +0400

    update example1 - example3

--- examples/js/example1.js
+++ examples/js/example1.js
@@ -1,10 +1,10 @@
-$(function(){
-    // Подключение автодополнения улиц
-    $( '[name="street"]' ).kladr({
-        token: '51dfe5d42fb2b43e3300006e',
-        key: '86a2c2a06f1b2451a87d05512cc2c3edfdf41969',
-        type: $.kladr.type.street,
-        parentType: $.kladr.type.city,
-        parentId: '7700000000000'
-    });
+$(function () {
+	// Подключение автодополнения улиц
+	$('[name="street"]').kladr({
+		token: '51dfe5d42fb2b43e3300006e',
+		key: '86a2c2a06f1b2451a87d05512cc2c3edfdf41969',
+		type: $.kladr.type.street,
+		parentType: $.kladr.type.city,
+		parentId: '7700000000000'
+	});
 });
