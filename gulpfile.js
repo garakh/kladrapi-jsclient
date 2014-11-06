536d21737ec5199730bc663309df1915166bc58d
xescoder <xescoder@gmail.com>
2014-11-06 16:49:29 +0400

    edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
 var gulp = require('gulp'),
 	csso = require('gulp-csso'),
-	imagemin = require('gulp-imagemin'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
 	rename = require('gulp-rename');
 
 gulp.task('default', function () {
-	gulp.src('./kladr/css/style.css')
-		.pipe(csso())
-		.pipe(rename('jquery.kladr.min.css'))
-		.pipe(gulp.dest('./'));
 
+	// Javascript
 	gulp.src([
 			'./kladr/js/core.js',
 			'./kladr/js/kladr.js',
@@ -19,5 +15,11 @@ gulp.task('default', function () {
 		.pipe(concat('jquery.kladr.js'))
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./'));
+
+	// CSS
+	gulp.src('./kladr/css/style.css')
+		.pipe(csso())
+		.pipe(rename('jquery.kladr.min.css'))
 		.pipe(gulp.dest('./'));
 });
